068a425541da93d4ecd6e00f9060e667c004c666
Netanel Maimon <netanel18999@gmail.com>
2025-01-12 12:20:02 +0200

    Add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import '../styles/globals.css'
 
@@ -5,6 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
       <SpeedInsights />
     </>
   )
