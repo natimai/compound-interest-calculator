33701e2111ec0871b0601630182d50c7850e7104
Netanel Maimon <netanel18999@gmail.com>
2025-01-12 15:43:00 +0200

    Add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="he" dir="rtl">
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WRNJLJQV12"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WRNJLJQV12');
+            `,
+          }}
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
