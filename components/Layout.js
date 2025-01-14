e0753a0c916af89efd5a1a1c8c6055441d5b0d15
Netanel Maimon <netanel18999@gmail.com>
2025-01-14 19:11:09 +0200

    Add accessibility script to Layout.js for enhanced functionality

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,16 @@
-// components/Layout.js
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
+import Head from 'next/head'
 
 export const Layout = ({ children }) => {
   return (
     <div dir="rtl" className="flex flex-col min-h-screen">
+      <Head>
+        <script
+          src="https://cdn.enable.co.il/licenses/enable-L36349gamb6znuqv-0125-67519/init.js"
+          async
+        ></script>
+      </Head>
       <Navbar />
       <main className="flex-grow">
         {children}
