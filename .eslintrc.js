4dbce71a8ca3154c073d37d653a1b2bb8286d272
Netanel Maimon <netanel18999@gmail.com>
2025-01-26 11:41:32 +0200

    תיקון בעיות SEO ובנייה: - תיקון קונפיגורציית sitemap - הוספת prebuild script לניקוי קבצים - עדכון .gitignore עבור קבצי sitemap - פישוט קונפיגורציית ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-img-element': 'off',
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [
+        ['next/babel']
+      ]
+    }
+  }
+};
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -14,5 +14,14 @@ module.exports = {
         },
       ],
     },
-    outDir: 'public/seo',
+    generateIndexSitemap: false,
+    transform: async (config, path) => {
+      return {
+        loc: path,
+        changefreq: config.changefreq,
+        priority: config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      }
+    },
 }
