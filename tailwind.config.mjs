4e1c34ad3f304d24453d78ce685a830f7b4b4b57
Netanel Maimon <netanel18999@gmail.com>
2025-03-27 19:20:57 +0200

    עדכון קבצי תצורה ומחיקת vercel.json

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        dark: {
+          DEFAULT: '#1a1a1a',
+          secondary: '#2d2d2d'
+        }
+      },
+    },
+  },
+  plugins: [],
+};
